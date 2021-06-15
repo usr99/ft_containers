da0a44df602335eac10963268136c33ef14f9951
Mael MARTIN <mamartin@e3r6p2.42.fr>
2021-06-15 09:49:48 +0200

    starting ft::list

---
+++ list.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamartin <mamartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/15 09:44:02 by mamartin          #+#    #+#             */
+/*   Updated: 2021/06/15 09:49:30 by mamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIST_HPP
+# define LIST_HPP
+
+namespace ft
+{
+	template <class T, class Alloc = allocator<T>>
+	class list
+	{
+		public:
+
+ 			// member types
+			typedef T		value_type;
+			typedef Alloc	allocator_type;
+
+		private:
+
+	};
+}
+
+#endif
